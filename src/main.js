2e73d1cda7516187869ea30aade5b5668363545e
Shubham Sharma <ssharma@0834@gmail.com>
2024-11-16 00:42:55 -0800

    Added Vuetify

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './vuetify'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router, vuetify)
 
 app.mount('#app')
